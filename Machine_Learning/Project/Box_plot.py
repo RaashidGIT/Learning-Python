db3052a055f3068312c59d49dfad30a54140f718
RaashidGIT <148008217+RaashidGIT@users.noreply.github.com>
2025-09-09 16:47:54 +0530

    Delete Machine_Learning/Project/Box_plot.py

--- Machine_Learning/Project/Box_plot.py
+++
@@ -1,6 +0,0 @@
-import seaborn as sns
-df = sns.load_dataset("tips")
-import matplotlib.pyplot as plt 
-sns.boxplot(x="day", y="total_bill", data=df,palette="pastel",hue="day",legend=False)
-plt.title("Box Plot")
-plt.show()
