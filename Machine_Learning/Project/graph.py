3b4db23a054fc9c176ac4f97dd61ed848b162fa8
RaashidGIT <148008217+RaashidGIT@users.noreply.github.com>
2025-09-09 15:23:33 +0530

    Create graph.py

---
+++ Machine_Learning/Project/graph.py
@@ -0,0 +1,27 @@
+import seaborn as sns
+import matplotlib.pyplot as plt  
+import pandas as pd
+
+# df = sns.load_dataset("tips")
+# sns.histplot(df["total_bill"], bins=20, kde=True, color='green')
+# plt.title("Histogram + KDE")
+# plt.show()
+
+# data = pd.DataFrame({
+#     "category": ['A', 'B', 'C', 'D'],
+#     "value": [4, 7, 2, 9]
+# })
+
+# sns.barplot(x="category", y="value", data=data)
+# plt.title("Normal Bar Chart (Seaborn)")
+# plt.show()
+
+# df=sns.load_dataset('tips')
+# sns.boxplot(x="day",y="total_bill",data=df,palette="pastel")
+# plt.title("box plot")
+# plt.show()
+
+df=sns.load_dataset('tips')
+sns.scatterplot(x="total_bill",y="tip",data=df,hue="sex",style="time")
+plt.title("Scatter plot")
+plt.show()
